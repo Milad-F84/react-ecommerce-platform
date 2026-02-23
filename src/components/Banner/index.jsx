c4dd312a7d22240fb87d14a70b5d46e39229dab2
Milad-F84 <mfarajollahifrontend@gmail.com>
2026-02-23 19:36:00 +0330

    feat: add banner for desctop version

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,1 +1,27 @@
-export default function Banner(){}
+import AppleIcon from '@mui/icons-material/Apple';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useNavigate } from "react-router-dom";
+
+export default function Banner(){
+    const navigate = useNavigate();
+    return(
+        <>
+            <div className='bg-[#000000] text-white flex justify-around items-center container-primary mt-7 mb-5'>
+                <div className='flex flex-col justify-center gap-2'>
+                    <div className='flex items-center gap-1'>
+                        <AppleIcon fontSize='large'/>
+                        <p>iPhone 14 Series</p>
+                    </div>
+                    <p className='text-6xl line-clamp-2 w-xl'>Up to 10% off Voucher</p>
+                    <div className='flex items-center gap-1 underline cursor-pointer'>
+                        <p onClick={() => navigate("/allproduct")}>Shop Now</p>
+                        <ArrowForwardIcon />
+                    </div>
+                </div>
+                <div>
+                    <img src="./banner.png" alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,1 +1,9 @@
-export default function HomePage(){}
+import Banner from "../components/Banner";
+
+export default function HomePage(){
+    return(
+        <>
+        <Banner />
+        </>
+    )
+}
